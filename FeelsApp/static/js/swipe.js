52ad7b5a95d850f462a0a73ca0039bc61975c510
aalbaharnah <baharnah.13@gmail.com>
2016-05-01 13:53:52 -0500

    recreate repo

---
+++ FeelsApp/static/js/swipe.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $(".buddy").on("swiperight",function(){
+      $(this).addClass('rotate-left').delay(700).fadeOut(1);
+      $('.buddy').find('.status').remove();
+
+      $(this).append('<div class="status like">Yes</div>');
+      if ( $(this).is(':last-child') ) {
+        $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+       } else {
+          $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+       }
+    });
+
+   $(".buddy").on("swipeleft",function(){
+    $(this).addClass('rotate-right').delay(700).fadeOut(1);
+    $('.buddy').find('.status').remove();
+    $(this).append('<div class="status dislike">NO</div>');
+
+    if ( $(this).is(':last-child') ) {
+     $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+      alert('You ran out of choices, fool!');
+     } else {
+        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+    }
+  });
+
+});
